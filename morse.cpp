6030df2c8fcb047c342aa8156af82479d6f8cb20
Franklin Wei <frankhwei536@gmail.com>
2014-04-06 21:58:39 +0100

    Made echoed characters all uppercase

--- morse.cpp
+++ morse.cpp
@@ -32,7 +32,7 @@ void transmit(const string& str)
       tmp=toupper(ch);
       string morse=table[tmp];
       if(morse!="")
-	cout << ch << ": " << flush;
+	cout << toupper(ch) << ": " << flush;
       for(unsigned int j=0;j<morse.length();++j)
 	{
 	  char morseCh=morse[j];
